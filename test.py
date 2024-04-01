91948b9e1f3c0ad27c5c499afcb7d55487ecc10c
AdonisCodes <business@simonferns.com>
2024-04-01 09:35:56 +0200

    Added Test Decorator

--- test.py
+++ test.py
@@ -2,23 +2,39 @@ import unittest
 from pixlr_private_api.main import PixlrApi
 
 
+def test_decorator(func):
+    def wrapper(*args, **kwargs):
+        print(f"==== Testing - {func.__name__} ====")
+        # We need 2 copies of the print function, to override and reset the print function
+        result = func(*args, **kwargs)
+        print("==== DONE  ====")
+        print("===============================\n\n")
+        return result
+
+    return wrapper
+
+
 class TestPixlrApi(unittest.TestCase):
+    @test_decorator
     def test_register(self):
         api = PixlrApi()
         assert api.register() is True
 
+    @test_decorator
     def test_verify(self):
         api = PixlrApi()
         assert api.register() is True
         assert api.verify_email() is True
         assert api.bearer_token is not None
 
+    @test_decorator
     def test_delete(self):
         api = PixlrApi()
         assert api.register() is True
         assert api.verify_email() is True
         assert api.delete_account() is True
 
+    @test_decorator
     def test_generate_image(self):
         api = PixlrApi()
         assert api.register() is True
@@ -28,16 +44,19 @@ class TestPixlrApi(unittest.TestCase):
         assert len(api.generate_image(768, 1344, 1, "An Image of a cute Bird!")) > 0
         api.delete_account()
 
+    @test_decorator
     def test_generate_phosus_token(self):
         api = PixlrApi()
         assert api._generate_phosus_auth_token() is not None
 
+    @test_decorator
     def test_remove_background(self):
         api = PixlrApi()
         image = api.remove_background("./dummy.jpg")
         print("test_remove_background IMAGE PATH: ", image)
         assert image is not None
 
+    @test_decorator
     def test_auto_fix(self):
         api = PixlrApi()
         image = api.auto_fix("./dummy.jpg")
